7b287370861f48b03a150935c7ea97412e665949
alex.popa31 <alex.popa@rawfish.com>
2024-03-15 22:55:19 +0100

    NavBar - behavior update
    Hamburger nav bar for small devices

--- js/navbar.js
+++ js/navbar.js
@@ -23,5 +23,68 @@ function interpolateColor(color1, color2, factor) {
     return result;
 }
 
+function selectNavItem(element, sectionId) {
+    // Rimuovi la classe 'selected' da tutti i link di navigazione
+    var navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach(function(link) {
+        link.classList.remove('selected');
+    });
+    
+    // Aggiungi la classe 'selected' al link cliccato
+    element.classList.add('selected');
+   
+    // Controllo sezione "home"
+    if (sectionId === 'home') {
+        // Effettua lo scroll fino all'inizio del viewport
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    } else {
+        // Controllo sezione valida
+        var section = document.getElementById(sectionId);
+        if (section) {
+            // Effettua lo scroll fino alla sezione desiderata
+            section.scrollIntoView({ behavior: 'smooth' });
+        } else {
+            // Sezione non trovata
+            console.log("Sezione non trovata:", sectionId);
+        }
+    }
+}
+
+
+
+  
+  // Funzione per evidenziare l'elemento della barra di navigazione corrispondente alla sezione attiva
+  function highlightNavItem(sectionId) {
+    const navItems = document.querySelectorAll('[data-section]');
+    navItems.forEach(item => {
+      if (item.getAttribute('data-section') === sectionId) {
+        item.classList.add('active');
+      } else {
+        item.classList.remove('active');
+      }
+    });
+  }
+
+  function handleScroll() {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        console.log(section.id, rect.top, rect.bottom, window.innerHeight);
+        if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+            highlightNavItem(section.id);
+        }
+    });
+}
+
+
+
+  // Gestione dello scrolling e dell'evidenziazione iniziale
+  document.addEventListener('scroll', handleScroll);
+  window.addEventListener('DOMContentLoaded', handleScroll);
+
+
 
   
